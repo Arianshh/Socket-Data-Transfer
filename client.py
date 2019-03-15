df4ab299fc8e8677dc0a616ba5acf1cb7095cb70
Aria <arian_fr2@yahoo.com>
2019-03-16 00:13:40 +0330

    Receive file ext via client

--- client.py
+++ client.py
@@ -1,14 +1,25 @@
 import socket
+from pathlib import Path
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.7', 1234))
 
 with open('received_file', 'wb') as received_data:
     while True:
+        ext_data = True
         print('Receiving data...')
-        data = s.recv(1024)
-        if not data:
-            break
-        received_data.write(data)
-    received_data.close()
-    s.close()
+        while True:
+            if ext_data:
+                ext = s.recv(3)
+                ext = ext.decode('utf-8')
+                ext_data = False
+            data = s.recv(1024)
+            if not data:
+                break
+            received_data.write(data)
+
+            p = Path('received_file')
+            p.rename(p.with_suffix('.' + ext))
+
+        received_data.close()
+        s.close()
