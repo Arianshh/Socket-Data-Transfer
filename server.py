8ec9a148f05402c3f9868db943714999323d278e
Aria <arian_fr2@yahoo.com>
2019-03-15 23:50:54 +0330

    Send file by server

--- server.py
+++ server.py
@@ -5,3 +5,20 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # establishing TCP connec
 host_IP = str(socket.gethostbyname(socket.gethostname()))
 s.bind((host_IP, 1234))
 s.listen(5)  # connection queue
+
+while True:
+    clientSock, clientAdd = s.accept()
+    print(f"Connection to {clientAdd[0]} has been established.")
+
+    # choose file to send.
+    filename = askopenfilename()
+    print(filename + ' uploaded.')
+    data = open(filename, 'rb')
+    data_to_bytes = data.read(1024)
+    while data_to_bytes:
+        clientSock.send(data_to_bytes)
+        data_to_bytes = data.read(1024)
+    data.close()
+
+    print('file sent.')
+    clientSock.close()
