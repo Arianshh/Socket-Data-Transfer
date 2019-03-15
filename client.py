7d94305862497ce578f3b9d35bf03cf04d040f91
Aria <arian_fr2@yahoo.com>
2019-03-16 00:17:38 +0330

    Fix client

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.1.7', 1234))
-
+ext = ''
 with open('received_file', 'wb') as received_data:
     while True:
         ext_data = True
@@ -18,8 +18,8 @@ with open('received_file', 'wb') as received_data:
                 break
             received_data.write(data)
 
-            p = Path('received_file')
-            p.rename(p.with_suffix('.' + ext))
-
         received_data.close()
+        p = Path('received_file')
+        p.rename(p.with_suffix('.' + ext))
+
         s.close()
