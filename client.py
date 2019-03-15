b5990e89b9b10e031fd25cdb79415e071d2c7dfb
Aria <arian_fr2@yahoo.com>
2019-03-15 23:55:06 +0330

    Receive file from client

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect('192.168.1.7')
+
+with open('received_file', 'wb') as received_data:
+    while True:
+        print('Receiving data...')
+        data = s.recv(1024)
+        if not data:
+            break
+        received_data.write(data)
+    received_data.close()
+    s.close()
