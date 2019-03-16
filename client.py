96949a8cab47a5f8f02f64e831a39b64359f0a14
Aria <arian_fr2@yahoo.com>
2019-03-16 13:41:00 +0330

    Beautify project

--- client.py
+++ client.py
@@ -2,24 +2,25 @@ import socket
 from pathlib import Path
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.1.7', 1234))
+s.connect(('', 1234))
 ext = ''
-with open('received_file', 'wb') as received_data:
+ext_data = True
+
+with open('Received_Files/received_file', 'wb') as received_data:
+    print('Receiving data...')
     while True:
-        ext_data = True
-        print('Receiving data...')
-        while True:
-            if ext_data:
-                ext = s.recv(3)
-                ext = ext.decode('utf-8')
-                ext_data = False
-            data = s.recv(1024)
-            if not data:
-                break
-            received_data.write(data)
+        if ext_data:
+            ext = s.recv(3)
+            ext = ext.decode('utf-8')
+            print(f"File Ext : {ext} extracted.")
+            ext_data = False
+        data = s.recv(1024)
+        if not data:
+            break
+        received_data.write(data)
 
-        received_data.close()
-        p = Path('received_file')
-        p.rename(p.with_suffix('.' + ext))
+    received_data.close()
+    p = Path('Received_Files/received_file')
+    p.rename(p.with_suffix('.' + ext))
 
-        s.close()
+    s.close()
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from tkinter.filedialog import askopenfilename
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # establishing TCP connection
 # host_IP = str(socket.gethostbyname(socket.gethostname()))
-s.bind(('192.168.1.7', 1234))
+s.bind(('', 1234))
 s.listen(5)  # connection queue
 
 while True:
