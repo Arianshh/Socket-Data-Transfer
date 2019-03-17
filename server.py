fd2fa14defbe266057bf2af9eb04ebd0a12c5157
Aria <arian_fr2@yahoo.com>
2019-03-17 14:24:07 +0330

    Add progressive bar to server

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from tkinter.filedialog import askopenfilename
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # establishing TCP connection
 # host_IP = str(socket.gethostbyname(socket.gethostname()))
-s.bind(('192.168.1.7', 1235))
+s.bind(('192.168.1.7', 1234))
 s.listen(5)  # connection queue
 
 while True:
@@ -28,9 +28,15 @@ while True:
     data_to_bytes = data.read(1024)
     bytes_transferred = bytes(0)
 
+    load = 0
+
     while data_to_bytes:
+        if load % 10000 == 0:
+            print(int(load*1024/data_size*100), ' % done')
+
         clientSock.send(data_to_bytes)
         data_to_bytes = data.read(1024)
+        load += 1
     data.close()
 
     print('file sent.')
